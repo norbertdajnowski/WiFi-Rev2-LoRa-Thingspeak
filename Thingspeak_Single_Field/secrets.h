85eb32408a9125cec360f03b975d7a40f266996d
norbertdajnowski <norbertdajnowski@gmail.com>
2021-11-12 17:17:29 +0000

    LoRa & Thingspeak Added

---
+++ Thingspeak_Single_Field/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "PLUSNET_JKR7"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "8ada5e2aeb"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 1569120			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "4K7W7VO0MP8KVRKY"   // replace XYZ with your channel write API Key
